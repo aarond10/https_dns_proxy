8dc5645fa3798b463121ba75f08b071f479a170a
baranyaib90 <5031516+baranyaib90@users.noreply.github.com>
2025-01-04 22:10:18 +0100

    Using wait for multiplexing over HTTP/1.x
    When a large burst of DNS requests came in and the
    only HTTPS/2 connection was too many time in idle state,
    curl closes it and opens brand new connection for every
    single DNS request.
    Obviously later it will keep only 1 and close all the others.
    So it is better to wait for multiplexing.
    When testing this setting, the DNS query latency values
    did not raise.

--- src/https_client.c
+++ src/https_client.c
@@ -315,7 +315,7 @@ static void https_fetch_ctx_init(https_client_t *client,
   ASSERT_CURL_EASY_SETOPT(ctx, CURLOPT_WRITEFUNCTION, &write_buffer);
   ASSERT_CURL_EASY_SETOPT(ctx, CURLOPT_WRITEDATA, ctx);
   ASSERT_CURL_EASY_SETOPT(ctx, CURLOPT_MAXAGE_CONN, client->opt->max_idle_time);
-  // FIXME: consider adding CURLOPT_PIPEWAIT
+  ASSERT_CURL_EASY_SETOPT(ctx, CURLOPT_PIPEWAIT, client->opt->use_http_version > 1);
   ASSERT_CURL_EASY_SETOPT(ctx, CURLOPT_USERAGENT, "https_dns_proxy/0.3");
   ASSERT_CURL_EASY_SETOPT(ctx, CURLOPT_FOLLOWLOCATION, 0);
   ASSERT_CURL_EASY_SETOPT(ctx, CURLOPT_NOSIGNAL, 0);
