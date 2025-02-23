fd62a31adc96610211e01c9fd23ecc205b4fd6ad
baranyaib90 <5031516+baranyaib90@users.noreply.github.com>
2025-02-23 23:06:14 +0100

    Update HTTP3 development build script

--- development_build_with_http3.sh
+++ development_build_with_http3.sh
@@ -7,10 +7,14 @@ echo "WARNING !!!"
 echo
 echo "Use only for development and testing!"
 echo "It is highly highly not recommended, to use in production!"
-echo "This script was based on: https://github.com/curl/curl/blob/curl-7_82_0/docs/HTTP3.md"
+echo "This script was based on: https://github.com/curl/curl/blob/curl-8_12_1/docs/HTTP3.md"
+echo
+echo "Extra packages suggested to be installed: autoconf libtool"
 echo
 
 sleep 5
+
+set -x
 
 INSTALL_DIR=$PWD/custom_curl/install
 mkdir -p $INSTALL_DIR
@@ -18,22 +22,23 @@ cd custom_curl
 
 ###
 
-git clone --depth 1 -b openssl-3.0.0+quic https://github.com/quictls/openssl
+git clone --depth 1 -b openssl-3.1.4+quic https://github.com/quictls/openssl
 cd openssl
 ./config enable-tls1_3 --prefix=$INSTALL_DIR
 make -j build_libs
 make install_dev
 cd ..
 
-git clone --depth 1 -b v0.3.0 https://github.com/ngtcp2/nghttp3
+git clone --depth 1 -b v1.1.0 https://github.com/ngtcp2/nghttp3
 cd nghttp3
+git submodule update --init
 autoreconf -fi
 ./configure --prefix=$INSTALL_DIR --enable-lib-only
 make -j
 make install
 cd ..
 
-git clone --depth 1 -b v0.3.1 https://github.com/ngtcp2/ngtcp2
+git clone --depth 1 -b v1.2.0 https://github.com/ngtcp2/ngtcp2
 cd ngtcp2
 autoreconf -fi
 ./configure PKG_CONFIG_PATH=$INSTALL_DIR/lib64/pkgconfig:$INSTALL_DIR/lib64/pkgconfig LDFLAGS="-Wl,-rpath,$INSTALL_DIR/lib64" --prefix=$INSTALL_DIR --enable-lib-only --with-openssl
@@ -41,7 +46,7 @@ make -j
 make install
 cd ..
 
-git clone --depth 1 -b v1.47.0 https://github.com/nghttp2/nghttp2
+git clone --depth 1 -b v1.64.0 https://github.com/nghttp2/nghttp2
 cd nghttp2
 autoreconf -fi
 ./configure PKG_CONFIG_PATH=$INSTALL_DIR/lib64/pkgconfig:$INSTALL_DIR/lib64/pkgconfig LDFLAGS="-Wl,-rpath,$INSTALL_DIR/lib64" --prefix=$INSTALL_DIR --enable-lib-only --with-openssl
@@ -49,7 +54,7 @@ make -j
 make install
 cd ..
 
-git clone --depth 1 -b curl-7_82_0 https://github.com/curl/curl
+git clone --depth 1 -b curl-8_12_1 https://github.com/curl/curl
 cd curl
 autoreconf -fi
 LDFLAGS="-Wl,-rpath,$INSTALL_DIR/lib64" ./configure --with-openssl=$INSTALL_DIR --with-nghttp2=$INSTALL_DIR --with-nghttp3=$INSTALL_DIR --with-ngtcp2=$INSTALL_DIR --prefix=$INSTALL_DIR
@@ -60,5 +65,5 @@ cd ..
 ###
 
 cd ..
-cmake -D CUSTOM_LIBCURL_INSTALL_PATH=$INSTALL_DIR .
+cmake -D CUSTOM_LIBCURL_INSTALL_PATH=$INSTALL_DIR -D CMAKE_BUILD_TYPE=Debug .
 make -j
