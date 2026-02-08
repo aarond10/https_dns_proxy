5fd40ad59eb18d5233682292e7d40f1da0688379
Aaron Drew <aarond10@gmail.com>
2026-02-08 21:44:55 +1100

    Fix memory leak in DNS response truncation
    Always free new_resp when non-NULL, regardless of size comparison.

--- src/dns_server.c
+++ src/dns_server.c
@@ -181,12 +181,14 @@ static void truncate_dns_response(char *buf, size_t *buflen, const uint16_t size
   }
   ares_dns_record_destroy(dnsrec);
 
-  if (new_resp != NULL && new_resp_len < old_size) {
-    memcpy(buf, new_resp, new_resp_len);
-    *buflen = new_resp_len;
-    buf[2] |= 0x02;  // set truncation flag
-    ILOG("%04hX: DNS response size truncated from %u to %u to keep %u limit",
-         tx_id, old_size, new_resp_len, size_limit);
+  if (new_resp != NULL) {
+    if (new_resp_len < old_size) {
+      memcpy(buf, new_resp, new_resp_len);
+      *buflen = new_resp_len;
+      buf[2] |= 0x02;  // set truncation flag
+      ILOG("%04hX: DNS response size truncated from %u to %u to keep %u limit",
+           tx_id, old_size, new_resp_len, size_limit);
+    }
     ares_free_string(new_resp);
   }
 }
